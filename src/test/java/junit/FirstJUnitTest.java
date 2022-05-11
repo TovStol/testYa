eac1e725bf2ffe8411de60e8ad22005fd177f21c
Aleksandr Tovstolik <alex_a_s@inbox.ru>
2022-05-11 07:30:14 +0500

    test datapicker

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,52 @@
+package junit;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
+
+public class FirstJUnitTest {
+    @BeforeAll
+    static void openBrowse() {
+        Selenide.open("https://demoqa.com/automation-practice-form");
+    }
+
+    @AfterAll
+    static void closeBrowse() {
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void firstTest() {
+
+        $(".form-label").shouldHave(text("Name"));
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Alex@Ivanov.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("9875995990");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOptionContainingText("1983");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("July");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__day--001").click();
+
+        //$("#dateOfBirthInput").click();
+
+
+
+    }
+
+
+}
