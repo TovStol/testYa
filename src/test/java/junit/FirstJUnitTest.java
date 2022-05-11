77a856d8056e6f5ca51c03a128d39775c39cde9b
TovStol <alex_a_s@inbox.ru>
2022-05-11 14:47:31 +0500

    add pressEnter

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,5 +1,6 @@
 package junit;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,7 +18,9 @@ import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 public class FirstJUnitTest {
     @BeforeAll
     static void openBrowse() {
+        Configuration.browserSize = "1920x1080";
         Selenide.open("https://demoqa.com/automation-practice-form");
+
     }
 
     @AfterAll
@@ -29,11 +32,15 @@ public class FirstJUnitTest {
     void firstTest() {
 
         $(".form-label").shouldHave(text("Name"));
+
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("Alex@Ivanov.com");
+
         $(byText("Male")).click();
+
         $("#userNumber").setValue("9875995990");
+
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOptionContainingText("1983");
@@ -42,8 +49,14 @@ public class FirstJUnitTest {
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__day--001").click();
 
-        //$("#dateOfBirthInput").click();
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+        $(byText("Reading")).click();
 
+
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $(byText("Rading")).click();
 
 
     }
