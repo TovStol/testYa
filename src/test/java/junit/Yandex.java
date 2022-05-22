b81e75247b033d9b8a6b343059b66a20bff25799
Aleksandr Tovstolik <alex_a_s@inbox.ru>
2022-05-23 00:17:39 +0500

    format text

--- src/test/java/junit/Yandex.java
+++ src/test/java/junit/Yandex.java
@@ -12,8 +12,7 @@ import java.security.Key;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.ALT;
-import static org.openqa.selenium.Keys.TAB;
+import static org.openqa.selenium.Keys.*;
 
 
 public class Yandex {
@@ -40,9 +39,48 @@ public class Yandex {
        $("[id='passp:sign-in']").click();
        $("[id='passp-field-passwd']").setValue("!N.surnametest!").pressEnter();
        $("[data-statlog='notifications.mail.login.inbox.unread']").shouldBe(visible).click();
+       switchTo().window(1);
 
-       $("[data-statlog='notifications.mail.login.inbox.unread']").sendKeys(TAB + ALT );
+       //Шаг 1: Нажать на кнопку Написать.
        $( "[href='#compose']").click();
+
+       //Ожидание: Открывается экран написания нового письма
+        $(".ComposePopup-Content").shouldHave(text("Новое письмо"));
+
+//Шаг 2: Заполнить поля Кому, Тема, в поле для ввода текста письма вставить
+//текст из нескольких абзацев.
+        // Заполнение поля адреса
+        $$(".MultipleAddressesDesktop-Field.ComposeYabblesField")
+                .first()
+                .lastChild()
+                .sendKeys("alex_a_s@inbox.ru");
+        //Заполнение поля темы
+        $("[name='subject']").setValue("Тестовое письмо");
+        //  Написание письма
+        $("#cke_1_contents")
+                .lastChild()
+                .setValue("В чужбине свято наблюдаю\n" +
+                        "Родной обычай старины:\n" +
+                        "На волю птичку выпускаю\n" +
+                        "При светлом празднике весны.\n\n" +
+                        "Я стал доступен утешенью;\n" +
+                        "За что на бога мне роптать,\n" +
+                        "Когда хоть одному творенью\n" +
+                        "Я мог свободу даровать!\n");
+/* К разным частям текста письма применить следующие виды форматирования:
+Жирный
+Подчеркивание
+Зачеркивание
+Курсив
+Маркированный список
+Нумерованный список */
+
+
+
+
+
+
+
 
 
 
