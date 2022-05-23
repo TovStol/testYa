0c8ee99add872a21f531f4a3dd447f15817adbc5
Aleksandr Tovstolik <alex_a_s@inbox.ru>
2022-05-23 23:05:30 +0500

    step 3

--- src/test/java/junit/Yandex.java
+++ src/test/java/junit/Yandex.java
@@ -1,5 +1,6 @@
 package junit;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +12,8 @@ import org.openqa.selenium.interactions.SendKeysAction;
 import java.security.Key;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.*;
 
@@ -38,25 +41,25 @@ public class Yandex {
        $("[id='passp-field-login']").setValue("n.surnametest");
        $("[id='passp:sign-in']").click();
        $("[id='passp-field-passwd']").setValue("!N.surnametest!").pressEnter();
-       $("[data-statlog='notifications.mail.login.inbox.unread']").shouldBe(visible).click();
+       $(".desk-notif-card__mail-title").shouldBe(visible).click();
        switchTo().window(1);
 
-       //Шаг 1: Нажать на кнопку Написать.
+//Шаг 1: Нажать на кнопку Написать.
        $( "[href='#compose']").click();
 
-       //Ожидание: Открывается экран написания нового письма
+//Ожидание: Открывается экран написания нового письма
         $(".ComposePopup-Content").shouldHave(text("Новое письмо"));
 
 //Шаг 2: Заполнить поля Кому, Тема, в поле для ввода текста письма вставить
 //текст из нескольких абзацев.
-        // Заполнение поля адреса
+// Заполнение поля адреса
         $$(".MultipleAddressesDesktop-Field.ComposeYabblesField")
                 .first()
                 .lastChild()
                 .sendKeys("alex_a_s@inbox.ru");
-        //Заполнение поля темы
+//Заполнение поля темы
         $("[name='subject']").setValue("Тестовое письмо");
-        //  Написание письма
+//Написание письма
         $("#cke_1_contents")
                 .lastChild()
                 .setValue("В чужбине свято наблюдаю\n" +
@@ -66,15 +69,43 @@ public class Yandex {
                         "Я стал доступен утешенью;\n" +
                         "За что на бога мне роптать,\n" +
                         "Когда хоть одному творенью\n" +
-                        "Я мог свободу даровать!\n");
-/* К разным частям текста письма применить следующие виды форматирования:
-Жирный
-Подчеркивание
-Зачеркивание
-Курсив
-Маркированный список
-Нумерованный список */
+                        "Я мог свободу даровать!");
+// К разным частям текста письма применить следующие виды форматирования:
+//Жирный
+    $(byText("В чужбине свято наблюдаю")).click();
+    $(byText("В чужбине свято наблюдаю")).sendKeys(SHIFT,HOME);
+    $(byText("В чужбине свято наблюдаю")).sendKeys(CONTROL, "b");
+//Подчеркивание
+    $(byText("Родной обычай старины:")).click();
+    $(byText("Родной обычай старины:")).sendKeys(SHIFT,HOME);
+    $(byText("Родной обычай старины:")).sendKeys(CONTROL, "u");
+//Зачеркивание
+        $(byText("На волю птичку выпускаю")).click();
+        $(byText("На волю птичку выпускаю")).sendKeys(SHIFT,HOME);
+        $("[data-cke-csp-href-useless-javascript=\"void('Перечёркнутый')\"]").click();
+//Курсив
+        $(byText("При светлом празднике весны.")).click();
+        $(byText("При светлом празднике весны.")).sendKeys(SHIFT,HOME);
+        $(byText("При светлом празднике весны.")).sendKeys(CONTROL, "i");
+//Маркированный список
+        $(byText("Я стал доступен утешенью;")).click();
+        $(byText("Я стал доступен утешенью;")).sendKeys(SHIFT,HOME);
+        $("[data-cke-csp-href-useless-javascript=\"void('Маркированный список')\"]").click();
+//Нумерованный список
+        $(byText("За что на бога мне роптать,")).click();
+        $(byText("За что на бога мне роптать,")).sendKeys(SHIFT,HOME);
+        $("[data-cke-csp-href-useless-javascript=\"void('Нумерованный список')\"]").click();
 
+
+//  Ожидание: Текст отображается с учетом форматирования.
+        $("strong").shouldHave(text("В чужбине свято наблюдаю"));
+        $("u").shouldHave(text("Родной обычай старины:"));
+        $("[style='text-decoration:line-through;']").shouldHave(text("На волю птичку выпускаю"));
+        $("em").shouldHave(text("При светлом празднике весны."));
+        $$("ul").shouldHave(CollectionCondition.itemWithText("Я стал доступен утешенью;"));
+        $$("ol").shouldHave(CollectionCondition.itemWithText("За что на бога мне роптать,"));
+
+//Шаг 3: Вставить валидные картинки выбором с компьютера и ссылкой.
 
 
 
